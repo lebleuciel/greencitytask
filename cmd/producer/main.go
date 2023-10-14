9522f12c736afeeb94b358522a76013ca8a94bc6
lebleuciel <93124243+lebleuciel@users.noreply.github.com>
2023-10-14 14:42:53 +0330

    add comments for producerr

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -10,21 +10,22 @@ import (
 )
 
 func main() {
+	// Create a new Sarama configuration for Kafka producer
 	config := sarama.NewConfig()
 	config.Producer.Return.Successes = true
 	config.Producer.Partitioner = sarama.NewManualPartitioner
-
+	// Initialize the Kafka producer to connect to Kafka broker at "kafka:9092"
 	producer, err := sarama.NewSyncProducer([]string{"kafka:9092"}, config)
 	if err != nil {
 		log.Fatal("Error initializing Kafka producer:", err)
 	}
 	defer producer.Close()
-
+	// Create a queue to store data
 	q := queue.New()
 
 	topic := "greencity-topic"
 	partitions := 4
-
+	// Start a Goroutine to generate and enqueue random data
 	go func() {
 		for i := 0; ; i++ {
 			data := generateRandomData()
@@ -38,11 +39,13 @@ func main() {
 	select {}
 }
 
+// sendMessagesToKafka sends enqueued data to Kafka with partitioning
 func sendMessagesToKafka(producer sarama.SyncProducer, topic string, partitions int, q *queue.Queue) {
 	for q.Length() > 0 {
 		msg := q.Peek()
 		for i := 0; i < 4; i++ {
 			if data, ok := msg.([]byte); ok {
+				// Prepare and send data to Kafka, partitioned
 				producerMessages := make([]*sarama.ProducerMessage, 4)
 				for partition := 0; partition < partitions; partition++ {
 					producerMessages[partition] = &sarama.ProducerMessage{
@@ -67,6 +70,7 @@ func sendMessagesToKafka(producer sarama.SyncProducer, topic string, partitions 
 	}
 }
 
+// generateRandomData generates random data from a predefined list of sentences
 func generateRandomData() []byte {
 	sentences := []string{
 		"Green",
@@ -80,7 +84,7 @@ func generateRandomData() []byte {
 	}
 
 	var data string
-
+	// Generate data by concatenating random sentences until the total length is greater than 320
 	for len(data) <= 320 {
 		randomIndex := rand.Intn(len(sentences))
 		data += sentences[randomIndex]
