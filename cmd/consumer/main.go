a4cfe2af1d6a9da3db06e62b1037483d2385fbdf
lebleuciel <93124243+lebleuciel@users.noreply.github.com>
2023-10-14 14:39:22 +0330

    add comments for consumer

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -11,15 +11,16 @@ import (
 )
 
 func main() {
+	// Create a new Sarama configuration for Kafka consumer
 	config := sarama.NewConfig()
 	config.Consumer.Offsets.Initial = sarama.OffsetOldest
-
+	// Initialize the Kafka consumer to connect to Kafka broker at "kafka:9092"
 	consumer, err := sarama.NewConsumer([]string{"kafka:9092"}, config)
 	if err != nil {
 		log.Fatal("Error creating Kafka consumer:", err)
 	}
 	defer consumer.Close()
-
+	// Define the Kafka topic to subscribe to and partitions to listen on
 	topic := "greencity-topic"
 	partitions := []int32{0, 1, 2, 3}
 
@@ -27,6 +28,7 @@ func main() {
 	receivedMessages := make(map[int32][]string)
 
 	fmt.Println("Kafka Consumer started. Listening for messages...")
+	// Iterate over specified partitions, create partition consumers, and start Goroutines
 	for _, partition := range partitions {
 		partitionConsumer, err := consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
 		if err != nil {
@@ -34,6 +36,7 @@ func main() {
 		}
 
 		go func(partition int32, partitionConsumer sarama.PartitionConsumer) {
+			// Continuously listen for and process Kafka messages in each partition
 			for msg := range partitionConsumer.Messages() {
 				mu.Lock()
 				receivedMessages[partition] = append(receivedMessages[partition], string(msg.Value))
@@ -42,10 +45,11 @@ func main() {
 			}
 		}(partition, partitionConsumer)
 	}
-
+	// The consumer runs indefinitely to keep listening for Kafka messages
 	select {}
 }
 
+// isVerified checks if a message contains only allowed words
 func isVerified(msg string) bool {
 	allowedWords := []string{
 		"Green",
@@ -64,6 +68,7 @@ func isVerified(msg string) bool {
 	return regex.MatchString(msg)
 }
 
+// verifyMessages checks and logs verification status for messages received from different partitions
 func verifyMessages(mu *sync.Mutex, receivedMessages map[int32][]string, partitions []int32) {
 	mu.Lock()
 	defer mu.Unlock()
